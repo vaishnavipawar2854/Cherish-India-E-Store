ba71acabdf1fec1103fb1897e66670971fa7f4a8
vaishnavipawar2854 <vaishnavipawar285042gmail.com>
2025-10-22 13:45:19 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://cherish-india-e-store-backend.vercel.app',
         changeOrigin: true,
       },
     },
